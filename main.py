548c5446600033b02014b4574751173fe3ae39f6
Cheesecake2960 <155858568+Cheesecake2960@users.noreply.github.com>
2024-07-31 08:23:38 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+try:
+  target = save = int(input('Please enter number! > '))
+  while not save == 1:
+    if save % 2 == 1:
+      save = save * 3 + 1
+    else:
+      save = save // 2
+    print(save)
+  print(f'target:{target} clear!')
+except:
+  print('It\'s not number!')
